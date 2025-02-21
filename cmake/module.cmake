4c1fcfecd1ce00671e2ccb22c15baeee395e6357
fruityloops1 <fruityloops@littun.co>
2025-02-21 23:15:26 +0100

    dont export symbols for debug (its Broken)

--- cmake/module.cmake
+++ cmake/module.cmake
@@ -39,10 +39,6 @@ endif()
 target_link_options(${PROJECT_NAME} PRIVATE ${LINKFLAGS} ${OPTIMIZE_OPTIONS})
 target_link_options(${PROJECT_NAME} PRIVATE -Wl,-init=__module_entry__ -Wl,--pie -Wl,--export-dynamic-symbol=_ZN2nn2ro6detail15g_pAutoLoadListE)
 
-if (CMAKE_BUILD_TYPE STREQUAL RelWithDebInfo OR CMAKE_BUILD_TYPE STREQUAL Debug)
-    target_link_options(${PROJECT_NAME} PRIVATE -Wl,--export-dynamic) # meh
-endif()
-
 target_compile_options(${PROJECT_NAME} PRIVATE
     $<$<COMPILE_LANGUAGE:ASM>:${ASM_OPTIONS}>
 )
@@ -94,7 +90,7 @@ else()
 endif()
 
 if (TARGET_IS_STATIC)
-    add_executable(rtld 
+    add_executable(rtld
     ${PROJECT_SOURCE_DIR}/sys/hakkun/src/rtld/DummyRtld.cpp
     ${PROJECT_SOURCE_DIR}/sys/hakkun/src/hk/init/module.S
     ${PROJECT_SOURCE_DIR}/sys/hakkun/src/hk/svc/api.S
