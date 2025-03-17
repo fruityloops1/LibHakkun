cc61fe284fbe250bfca723aa3fbc7ee40e9e1e8f
fruityloops1 <fruityloops@littun.co>
2025-03-17 22:41:04 +0100

    dynamic heap source addon + sail for addons

--- cmake/addons.cmake
+++ cmake/addons.cmake
@@ -6,10 +6,15 @@ function(enable_addons)
         endif()
 
         add_subdirectory(${ADDON_DIR})
+        target_include_directories(LibHakkun PRIVATE ${ADDON_DIR}/include)
         target_include_directories(${PROJECT_NAME} PRIVATE ${ADDON_DIR}/include)
         target_include_directories(${addon} PRIVATE ${CMAKE_CURRENT_SOURCE_DIR}/sys/hakkun/include)
         target_link_libraries(${PROJECT_NAME} PRIVATE ${addon})
 
+        target_compile_definitions(${PROJECT_NAME} PRIVATE HK_ADDON_${addon})
+        target_compile_definitions(${addon} PRIVATE HK_ADDON_${addon})
+        target_compile_definitions(LibHakkun PRIVATE HK_ADDON_${addon})
+        
         message("Enabled addon ${addon}")
     endforeach()
 
