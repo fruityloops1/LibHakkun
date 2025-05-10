960966a683e2cc1aeb46a247af7f0a621207fe8c
fruityloops1 <fruityloops@littun.co>
2025-05-10 12:47:11 +0200

    Replace bin2s with CMake rewrite

--- cmake/bin2s.cmake
+++ cmake/bin2s.cmake
@@ -1,21 +1,30 @@
-file(TO_CMAKE_PATH "$ENV{BIN2S}" BIN2S)
+cmake_minimum_required(VERSION 3.13)
 
-if(NOT EXISTS ${BIN2S})
-    if(NOT IS_DIRECTORY ${DEVKITPRO})
-        message(FATAL_ERROR "Please install devkitA64 or set BIN2S in your environment.")
-    else()
-        set(BIN2S ${DEVKITPRO}/tools/bin/bin2s)
-    endif()
-endif()
-
-function(embed_file project filepath filename)
-    set(SPATH ${CMAKE_CURRENT_BINARY_DIR}/${filename}_shader.S)
-
-    add_custom_command(OUTPUT ${SPATH} PRE_BUILD
-        COMMAND ${BIN2S} ${filepath} --header ${CMAKE_CURRENT_BINARY_DIR}/embed_${filename}.h > ${SPATH} && sed -i -f ${ROOTDIR}/sys/data/replace_bin2s.sed ${SPATH}
-        DEPENDS ${filepath}
-        )
-    set_source_files_properties(${SPATH} PROPERTIES GENERATED TRUE)
-    target_sources(${project} PRIVATE ${SPATH})
-    target_include_directories(${project} PRIVATE ${CMAKE_CURRENT_BINARY_DIR})
+function(embed_file PROJECT_NAME INPUT_FILE FILENAME ALIGN IS_DATA)
+  set(SPATH ${CMAKE_CURRENT_BINARY_DIR}/${FILENAME}_bin2s.S)
+  set(HEADER_FILE ${CMAKE_CURRENT_BINARY_DIR}/embed_${FILENAME}.h)
+  
+  get_filename_component(FILENAME ${INPUT_FILE} NAME)
+  
+  string(REGEX REPLACE "[^a-zA-Z0-9_]" "_" SYMBOL_NAME ${FILENAME})
+  string(REGEX REPLACE "^[0-9]" "_\\0" SYMBOL_NAME ${SYMBOL_NAME})
+  
+  add_custom_command(
+    OUTPUT ${SPATH} ${HEADER_FILE}
+    COMMAND ${CMAKE_COMMAND}
+      -DINPUT_FILE=${INPUT_FILE}
+      -DOUTPUT_ASM_FILE=${SPATH}
+      -DOUTPUT_HEADER_FILE=${HEADER_FILE}
+      -DSYMBOL_NAME=${SYMBOL_NAME}
+      -DALIGNMENT=${ALIGN}
+      -DIS_DATA=${IS_DATA}
+      -P ${ROOTDIR}/sys/cmake/impl/bin2s.cmake
+    DEPENDS ${INPUT_FILE}
+    VERBATIM
+  )
+  
+  target_sources(${PROJECT_NAME} PRIVATE ${SPATH})
+  
+  get_filename_component(HEADER_DIR ${HEADER_FILE} DIRECTORY)
+  target_include_directories(${PROJECT_NAME} PRIVATE ${HEADER_DIR})
 endfunction()
