2db9418b043f7f8e1eff08add19642a7b002aad8
fruityloops1 <fruityloops@littun.co>
2025-08-17 16:20:48 +0200

    util: Move StreamWriter into Stream

---
+++ hakkun/include/hk/util/Stream.h
@@ -0,0 +1,114 @@
+#pragma once
+
+#include "hk/diag/diag.h"
+#include "hk/types.h"
+#include <type_traits>
+
+namespace hk::util {
+
+    template <typename BufferType>
+    class Stream {
+        ptr mBuffer = 0;
+        size mSize = -1;
+        size mCursor = 0;
+
+        bool hasSize() const {
+            return mSize != -1;
+        }
+
+        constexpr static bool cIsReadOnly = std::is_const_v<BufferType>;
+
+        void checkReadonly() const {
+            HK_ABORT_UNLESS(cIsReadOnly == false, "hk::util::Stream: stream is readonly", 0);
+        }
+
+    public:
+        Stream(BufferType* buffer, size maxSize = -1)
+            : mBuffer(ptr(buffer))
+            , mSize(maxSize) { }
+
+        void seek(size cursor) {
+            if (hasSize())
+                HK_ABORT_UNLESS(cursor <= mSize, "hk::util::Stream::seek: out of range (%zu/%zu)", cursor, mSize);
+
+            mCursor = cursor;
+        }
+
+        size tell() const { return mCursor; }
+
+        bool tryWrite(void* data, size dataSize) {
+            checkReadonly();
+
+            if (hasSize() && mCursor + dataSize > mSize)
+                return false;
+
+            memcpy(cast<void*>(mBuffer + mCursor), data, dataSize);
+            mCursor += dataSize;
+
+            return true;
+        }
+
+        void write(void* data, size dataSize) {
+            checkReadonly();
+
+            if (hasSize())
+                HK_ABORT_UNLESS(mCursor + dataSize <= mSize, "hk::util::Stream::write: out of range (%zu/%zu)", mCursor + dataSize, mSize);
+
+            memcpy(cast<void*>(mBuffer + mCursor), data, dataSize);
+            mCursor += dataSize;
+        }
+
+        template <typename T>
+        void write(const T& value) {
+            write(&value, sizeof(T));
+        }
+
+        template <typename T>
+        bool tryWrite(const T& value) {
+            return tryWrite(&value, sizeof(T));
+        }
+
+        template <typename CT, typename T>
+        void writeIterator(const T& container) {
+            for (CT ct : container)
+                write(ct);
+        }
+
+        template <typename CT, typename T>
+        bool tryWriteIterator(const T& container) {
+            for (CT ct : container)
+                if (tryWrite(ct) == false)
+                    return false;
+            return true;
+        }
+
+        bool tryRead(void* out, size dataSize) {
+            if (hasSize() && mCursor + dataSize > mSize)
+                return false;
+
+            memcpy(out, cast<const void*>(mBuffer + dataSize), dataSize);
+            return true;
+        }
+
+        void read(void* out, size dataSize) {
+            if (hasSize())
+                HK_ABORT_UNLESS(mCursor + dataSize <= mSize, "hk::util::Stream::write: out of range (%zu/%zu)", mCursor + dataSize, mSize);
+
+            memcpy(out, cast<const void*>(mBuffer + dataSize), dataSize);
+        }
+
+        template <typename T>
+        T read() {
+            T value;
+            read(&value, sizeof(T));
+        }
+
+        template <typename T>
+        bool tryRead(T* out) {
+            return tryRead(out, sizeof(T));
+        }
+
+        static constexpr bool isReadOnly() { return cIsReadOnly; }
+    };
+
+} // namespace hk::util
