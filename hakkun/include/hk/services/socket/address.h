3b5ad5fec78d8c3445c8afe9c69f478f64675b85
Aubrey Taylor <aubrey@hall.ly>
2025-08-22 00:01:55 -0600

    simple working* sockets (qlaunch freezes on second run)

--- hakkun/include/hk/services/socket/address.h
+++ hakkun/include/hk/services/socket/address.h
@@ -8,35 +8,43 @@
 #include "hk/util/TemplateString.h"
 #include <bit>
 namespace hk::socket {
-    enum class AddressFamily : u32
+    enum class AddressFamily : u8
     {
         Ipv4 = 2,
         Ipv6 = 28,
     };
-    enum class Type : u32
+    enum class Type : u8
     {
         Stream = 1,
         Datagram = 2,
         Raw = 3,
         SequencedPacketStream = 5,
     };
-    enum class Protocol : u32
+    enum class Protocol : u8
     {
         Tcp = 2,
         Udp = 17,
     };
 
     class SocketAddr {
+        u8 mLength;
         AddressFamily mFamily;
+
+        friend class SocketAddrIpv4;
+        SocketAddr(u8 length, AddressFamily family)
+            : mLength(length)
+            , mFamily(family) { }
     };
     class SocketAddrIpv4 : public SocketAddr {
         constexpr SocketAddrIpv4(u16 port, u32 address)
-            : mPort(std::byteswap(port))
-            , mAddress(std::byteswap(address)) { }
+            : SocketAddr(6, AddressFamily::Ipv4)
+            , mPort(std::byteswap(port))
+            , mAddress(address) { }
 
     public:
         u16 mPort;
         u32 mAddress;
+        u64 _ = {};
         constexpr static ValueOrResult<u32> parseAddress(const char* text) {
             size len = __builtin_strlen(text);
             HK_UNLESS(len <= 15, ResultTooLong());
@@ -72,16 +80,13 @@ namespace hk::socket {
                 octets[i] = result.value();
                 value2 <<= 8;
                 value2 |= octets[i];
-                diag::debugLog("octet %x %d", octets[i], octets[i]);
-                diag::debugLog("value %x", value2);
             }
-
 
             return std::byteswap(value2);
         }
         template <util::TemplateString address>
         constexpr static SocketAddrIpv4 parse(u16 port) {
-            // static_assert(__builtin_strlen(address.value) <= 15, "IPv4 addresses are no longer than 15 characters");
+            static_assert(__builtin_strlen(address.value) <= 15, "IPv4 addresses are no longer than 15 characters");
             constexpr ValueOrResult<u32> result = parseAddress(address.value);
             static_assert(Result(result).getDescription() == 0);
 
--- hakkun/include/hk/services/socket/service.h
+++ hakkun/include/hk/services/socket/service.h
@@ -31,14 +31,11 @@ namespace hk::socket {
 
         void registerClient(const ServiceConfig& config, const u8* socketBuffer, size socketBufferSize) {
             Handle handle;
-            diag::debugLog("size %x", socketBufferSize);
             HK_ABORT_UNLESS_R(svc::CreateTransferMemory(&handle, ptr(socketBuffer), socketBufferSize, svc::MemoryPermission_None));
-            auto input = sf::packInput(config, u64(0), u64(socketBufferSize));
+            std::array<u8, 0x30> input = sf::packInput(config, u64(0xDEADBEEFCAFEBABE), u64(socketBufferSize));
             auto request = sf::Request(0, &input);
             request.addCopyHandle(handle);
             request.setSendPid();
-            // request.debugAbortAfterRequest();
-            request.enableDebug(true, true);
             HK_ABORT_UNLESS_R(invokeRequest(std::move(request)));
         }
 
@@ -48,7 +45,7 @@ namespace hk::socket {
         };
 
         Ret socket(AddressFamily domain, Type type, Protocol protocol) {
-            auto input = sf::packInput(domain, type, protocol);
+            std::array<u8, 12> input = sf::packInput(u32(domain), u32(type), u32(protocol));
             return HK_UNWRAP(invokeRequest(sf::Request(2, &input), sf::simpleDataHandler<Ret>()));
         }
 
@@ -61,13 +58,15 @@ namespace hk::socket {
         Ret connect(s32 fd, const A& address) {
         }
 
-        template <typename A, typename B>
-            requires(std::is_convertible<A*, SocketAddr*>::value)
-        Ret sendTo(s32 fd, std::span<const B> data, s32 flags, const A& address) {
+        template <typename B>
+        // requires(std::is_convertible_v<A*, SocketAddr*>)
+        Ret sendTo(s32 fd, std::span<const B> data, s32 flags, const SocketAddrIpv4& address) {
             auto input = sf::packInput(fd, flags);
+            diag::debugLog("sized %x %x %x %x", fd, flags, input.size(), address);
             auto request = sf::Request(11, &input);
             request.addInAutoselect(data.data(), data.size_bytes());
-            request.addInAutoselect(&address, sizeof(A));
+            request.addInAutoselect(&address, sizeof(SocketAddrIpv4));
+            request.enableDebug(true, true);
             return HK_UNWRAP(invokeRequest(std::move(request), sf::simpleDataHandler<Ret>()));
         }
     };
