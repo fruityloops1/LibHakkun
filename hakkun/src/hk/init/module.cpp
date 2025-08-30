87087aaaae9138f5c24bdbed28bf27e414cf8e07
fruityloops1 <fruityloops@littun.co>
2025-08-30 16:04:08 +0200

    Add main thread name

--- hakkun/src/hk/init/module.cpp
+++ hakkun/src/hk/init/module.cpp
@@ -54,6 +54,8 @@ namespace nn::init {
         // if (notifyExceptionHandlerReady)
         //     notifyExceptionHandlerReady();
 
+        hk::diag::setCurrentThreadName("HkMain");
+
         if (callInitializers)
             callInitializers();
 
