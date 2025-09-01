1d1acea3fc47079edcfd22e64e9017d9d9d3bffd
tetraxile <tetraxile@proton.me>
2025-09-01 20:19:34 +0100

    services: add pm:dmnt IPC commands

--- hakkun/include/hk/services/pm.h
+++ hakkun/include/hk/services/pm.h
@@ -3,6 +3,7 @@
 #include "hk/services/sm.h"
 #include "hk/sf/sf.h"
 #include "hk/sf/utils.h"
+#include "hk/types.h"
 #include "hk/util/Singleton.h"
 
 namespace hk::pm {
@@ -17,13 +18,41 @@ namespace hk::pm {
             return instance();
         }
 
+        size getJitDebugProcessIdList(std::span<u64> debugProcesses) {
+            auto request = sf::Request(this, 1);
+            request.addOutMapAlias(debugProcesses.data(), debugProcesses.size_bytes());
+            HK_ABORT_UNLESS_R(invokeRequest(move(request)));
+            return debugProcesses.size();
+        }
+
+        void startProcess(u64 pid) {
+            HK_ABORT_UNLESS_R(invokeRequest(sf::Request(this, 1, &pid)));
+        }
+
+        u64 getProcessId(u64 titleId) {
+            return HK_UNWRAP(sf::invokeSimple<u64>(*this, 2, titleId));
+        }
+
+        Handle hookToCreateProcess(u64 titleId) {
+            auto request = sf::Request(this, 3, &titleId);
+            return HK_UNWRAP(invokeRequest(move(request), [](sf::Response& response){ return response.nextCopyHandle(); }));
+        }
+
         u64 getApplicationProcessId() {
-            auto request = sf::Request(this, 4);
-            request.enableDebug(true, true);
-            return invokeRequest(move(request), [](sf::Response& response) {
-                HK_ASSERT(response.data.size_bytes() >= 8);
-                return sf::simpleDataHandler<u64>()(response);
-            });
+            return HK_UNWRAP(sf::invokeSimple<u64>(*this, 4));
+        }
+    
+        Handle hookToCreateApplicationProcess() {
+            auto request = sf::Request(this, 5);
+            return HK_UNWRAP(invokeRequest(move(request), [](sf::Response& response){ return response.nextCopyHandle(); }));
+        }
+
+        void clearHook(u32 bitflags) {
+            HK_ABORT_UNLESS_R(invokeRequest(sf::Request(this, 6, &bitflags)));
+        }
+
+        u64 getProgramId(u64 pid) {
+            return HK_UNWRAP(sf::invokeSimple<u64>(*this, 7, pid));
         }
     };
 
