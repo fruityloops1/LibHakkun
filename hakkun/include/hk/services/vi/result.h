6f0f7e32a7d611e9310764bd41d6a3f26f8e910c
tetraxile <tetraxile@proton.me>
2025-09-06 02:46:22 +0100

    vi: add ApplicationDisplayService command wrappers

---
+++ hakkun/include/hk/services/vi/result.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include "hk/Result.h"
+
+namespace hk::vi {
+
+    HK_RESULT_MODULE(412)
+    HK_DEFINE_RESULT_RANGE(Vi, 51, 69)
+
+    HK_DEFINE_RESULT(NotInitialized, 51)
+
+} // namespace hk::vi
---
+++ hakkun/src/hk/services/vi.cpp
@@ -0,0 +1,47 @@
+#include "hk/services/vi/service.h"
+
+namespace hk::vi {
+    HK_SINGLETON_IMPL(VideoInterface);
+    HK_SINGLETON_IMPL(ApplicationDisplayService);
+    HK_SINGLETON_IMPL(SystemDisplayService);
+    HK_SINGLETON_IMPL(ManagerDisplayService);
+
+    void initialize() {
+        VideoInterface::initialize();
+    }
+
+    Result openDefaultDisplay() {
+        return ApplicationDisplayService::instance()->openDefaultDisplay();
+    }
+
+    size listDisplays(std::span<DisplayInfo> displays) {
+        return ApplicationDisplayService::instance()->listDisplays(displays);
+    }
+
+    Result Display::open() {
+        auto rc = ApplicationDisplayService::instance()->openDisplay(*this);
+        return rc.map([this](u64 id){ this->id = id; });
+    }
+
+    Result Display::close() {
+        return ApplicationDisplayService::instance()->closeDisplay(*this);
+    }
+
+    Result Display::setEnabled(bool enabled) {
+        return ApplicationDisplayService::instance()->setDisplayEnabled(*this, enabled);
+    }
+
+    ValueOrResult<Tuple<u64, u64>> Display::getResolution() {
+        return ApplicationDisplayService::instance()->getDisplayResolution(*this);
+    }
+
+    ValueOrResult<Handle> Display::getVsyncEvent() {
+        return ApplicationDisplayService::instance()->getDisplayVsyncEvent(*this);
+    }
+
+    ValueOrResult<Handle> Display::getVsyncEventForDebug() {
+        return ApplicationDisplayService::instance()->getDisplayVsyncEventForDebug(*this);
+    }
+
+
+} // namespace hk::vi
