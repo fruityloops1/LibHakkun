008af21110b91385eca75016ff61f7a6db409857
fruityloops1 <fruityloops@littun.co>
2026-03-24 17:27:33 +0100

    span conversions

--- hakkun/include/hk/util/Span.h
+++ hakkun/include/hk/util/Span.h
@@ -4,46 +4,86 @@
 #include "hk/diag/diag.h"
 #include "hk/types.h"
 #include "hk/util/Allocator.h"
+#include <span>
+#include <type_traits>
 
 namespace hk::util {
 
-    template <typename T>
-    class Span {
-    protected:
-        T* mPtr = nullptr;
-        size mSize = 0;
+    namespace detail {
 
+        template <typename T>
+        class SpanBase {
+        protected:
+            T* mPtr = nullptr;
+            size mSize = 0;
+
+        public:
+            SpanBase() = default;
+            SpanBase(T* ptr, size size)
+                : mPtr(ptr)
+                , mSize(size) { }
+
+            void set(T* ptr, size size) {
+                mPtr = ptr;
+                mSize = size;
+            }
+
+            T* data() { return mPtr; }
+            const T* data() const { return mPtr; }
+            size size_bytes() const { return mSize * sizeof(T); }
+            size size() const { return mSize; }
+            bool empty() const { return mSize == 0; }
+
+            T& operator[](::size index) {
+                HK_ABORT_UNLESS(index < mSize, "hk::util::Span<%s>::operator[%zu]: out of range (size: %zu)", index, mSize);
+                return mPtr[index];
+            }
+
+            const T& operator[](::size index) const {
+                HK_ABORT_UNLESS(index < mSize, "hk::util::Span<%s>::operator[%zu]: out of range (size: %zu)", index, mSize);
+                return mPtr[index];
+            }
+
+            T* begin() { return &mPtr[0]; }
+            T* end() { return &mPtr[mSize]; }
+            const T* begin() const { return &mPtr[0]; }
+            const T* end() const { return &mPtr[mSize]; }
+        };
+
+    } // namespace detail
+
+    template <typename T>
+    class Span : public detail::SpanBase<T> {
     public:
         Span() = default;
         Span(T* ptr, size size)
-            : mPtr(ptr)
-            , mSize(size) { }
+            : detail::SpanBase<T>(ptr, size) { }
 
-        void set(T* ptr, size size) {
-            mPtr = ptr;
-            mSize = size;
-        }
+        Span(const Span<const T>& other) = delete;
+        Span(const Span& other)
+            : detail::SpanBase<T>(other.mPtr, other.mSize) { }
 
-        T* data() { return mPtr; }
-        const T* data() const { return mPtr; }
-        size size_bytes() const { return mSize * sizeof(T); }
-        size size() const { return mSize; }
-        bool empty() const { return mSize == 0; }
+        Span(const std::span<const T>& other) = delete;
+        Span(const std::span<T>& other)
+            : detail::SpanBase<T>(other.data(), other.size()) { }
+    };
 
-        T& operator[](::size index) {
-            HK_ABORT_UNLESS(index < mSize, "hk::util::Span<%s>::operator[%zu]: out of range (size: %zu)", index, mSize);
-            return mPtr[index];
-        }
+    template <typename T>
+    class Span<const T> : public detail::SpanBase<const T> {
+    public:
+        Span() = default;
+        Span(const T* ptr, size size)
+            : detail::SpanBase<const T>(ptr, size) { }
 
-        const T& operator[](::size index) const {
-            HK_ABORT_UNLESS(index < mSize, "hk::util::Span<%s>::operator[%zu]: out of range (size: %zu)", index, mSize);
-            return mPtr[index];
-        }
+        Span(const Span<T>& other)
+            : detail::SpanBase<const T>(other.mPtr, other.mSize) { }
+        Span(const Span& other)
+            : detail::SpanBase<const T>(other.mPtr, other.mSize) { }
 
-        T* begin() { return &mPtr[0]; }
-        T* end() { return &mPtr[mSize]; }
-        const T* begin() const { return &mPtr[0]; }
-        const T* end() const { return &mPtr[mSize]; }
+        Span(const std::span<const T>& other)
+            : detail::SpanBase<const T>(other.data(), other.size()) { }
+        Span(const std::span<T>& other)
+            : detail::SpanBase<const T>(other.data(), other.size()) { }
     };
 
 } // namespace hk::util
