0205e5a68f1d7a22f0e8ceff413eeec1aff42005
fruityloops1 <fruityloops@littun.co>
2026-04-23 01:58:09 +0200

    add various type traits, adjust types.h

---
+++ hakkun/core/include/hk/prim/traits/Type.h
@@ -0,0 +1,297 @@
+#pragma once
+
+namespace hk::util {
+
+    template <typename... Types>
+    struct TypeTraits;
+
+    template <typename T>
+    struct TypeTraits<T> {
+        template <bool Value>
+        struct Bool {
+            static constexpr bool cValue = Value;
+        };
+
+        using True = Bool<true>;
+        using False = Bool<false>;
+
+        template <typename C>
+        struct Same : False { };
+        template <>
+        struct Same<T> : True { };
+        template <typename Derived>
+        struct BaseOf : Bool<__is_base_of(T, Derived)> { };
+        struct Reference;
+        struct Pointer : Bool<__is_pointer(T)> { };
+        struct Const : Bool<__is_const(T)> { };
+        template <typename... Args>
+        struct TriviallyConstructible : Bool<__is_trivially_constructible(T, Args...)> { };
+        struct TriviallyDefaultConstructible : TriviallyConstructible<> { };
+        struct TriviallyMoveConstructible : TriviallyConstructible<T&&> { };
+        struct TriviallyCopyConstructible : TriviallyConstructible<const T&> { };
+        struct TriviallyDestructible : Bool<__is_trivially_destructible(T)> { };
+        struct Integral;
+        struct FloatingPoint;
+
+        template <typename _Tp>
+        struct _Type {
+            using Type = _Tp;
+        };
+
+        struct RemoveReference;
+        struct RemovePointer : _Type<__remove_pointer(T)> { };
+        struct RemoveConst : _Type<__remove_const(T)> { };
+
+        struct PrintfFormatVerbose;
+    };
+
+    template <typename T, typename C>
+    using ttSame = TypeTraits<T>::template Same<C>;
+    template <typename Base, typename Derived>
+    using ttBaseOf = TypeTraits<Base>::template BaseOf<Derived>;
+    template <typename T>
+    using ttReference = TypeTraits<T>::Reference;
+    template <typename T>
+    using ttPointer = TypeTraits<T>::Pointer;
+    template <typename T>
+    using ttConst = TypeTraits<T>::Const;
+    template <typename T, typename... Args>
+    using ttTriviallyConstructible = TypeTraits<T>::template TriviallyConstructible<Args...>;
+    template <typename T>
+    using ttTriviallyDefaultConstructible = TypeTraits<T>::TriviallyDefaultConstructible;
+    template <typename T>
+    using ttTriviallyMoveConstructible = TypeTraits<T>::TriviallyMoveConstructible;
+    template <typename T>
+    using ttTriviallyCopyConstructible = TypeTraits<T>::TriviallyCopyConstructible;
+    template <typename T>
+    using ttTriviallyDestructible = TypeTraits<T>::TriviallyDestructible;
+    template <typename T>
+    using ttIntegral = TypeTraits<T>::Integral;
+    template <typename T>
+    using ttFloatingPoint = TypeTraits<T>::FloatingPoint;
+
+    template <typename T, typename C>
+    constexpr bool ctIsSame = ttSame<T, C>::cValue;
+    template <typename Base, typename Derived>
+    constexpr bool ctIsBaseOf = ttBaseOf<Base, Derived>::cValue;
+    template <typename T>
+    constexpr bool ctIsReference = ttReference<T>::cValue;
+    template <typename T>
+    constexpr bool ctIsPointer = ttPointer<T>::cValue;
+    template <typename T>
+    constexpr bool ctIsConst = ttConst<T>::cValue;
+    template <typename T, typename... Args>
+    constexpr bool ctIsTriviallyConstructible = ttTriviallyConstructible<T, Args...>::cValue;
+    template <typename T>
+    constexpr bool ctIsTriviallyDefaultConstructible = ttTriviallyDefaultConstructible<T>::cValue;
+    template <typename T>
+    constexpr bool ctIsTriviallyMoveConstructible = ttTriviallyMoveConstructible<T>::cValue;
+    template <typename T>
+    constexpr bool ctIsTriviallyCopyConstructible = ttTriviallyCopyConstructible<T>::cValue;
+    template <typename T>
+    constexpr bool ctIsTriviallyDestructible = ttTriviallyDestructible<T>::cValue;
+    template <typename T>
+    constexpr bool ctIsIntegral = ttIntegral<T>::cValue;
+    template <typename T>
+    constexpr bool ctIsFloatingPoint = ttFloatingPoint<T>::cValue;
+
+    template <typename T>
+    using tRemoveReference = TypeTraits<T>::RemoveReference::Type;
+    template <typename T>
+    using tRemovePointer = TypeTraits<T>::RemovePointer::Type;
+    template <typename T>
+    using tRemoveConst = TypeTraits<T>::RemoveConst::Type;
+
+    template <typename T>
+    constexpr const char* ctPrintfFormatVerbose = TypeTraits<T>::PrintfFormatVerbose::cValue;
+
+    template <typename... Types>
+    struct TypeTraits {
+        template <template <typename> typename Trait>
+        static constexpr bool all() {
+            bool satisfied = true;
+            ([&] {
+                using Cur = Types;
+
+                satisfied &= Trait<Cur>::cValue;
+            }(),
+                ...);
+
+            return satisfied;
+        }
+
+        template <typename T>
+        static constexpr int findIndex() {
+            int index = 0;
+            int foundIdx = -1;
+            ([&] {
+                using Cur = Types;
+                if (ctIsSame<T, Cur>)
+                    foundIdx = index;
+                index++;
+            }(),
+                ...);
+            return foundIdx;
+        }
+
+        template <typename T>
+        static constexpr bool contains() {
+            bool found = false;
+            ([&] {
+                using Cur = Types;
+                if (ctIsSame<T, Cur>)
+                    found = true;
+            }(),
+                ...);
+
+            return found;
+        }
+
+        static constexpr bool amount() {
+            int amount = 0;
+            ([&] {
+                using Cur = Types;
+                amount++;
+            }(),
+                ...);
+
+            return amount;
+        }
+    };
+
+    namespace detail {
+
+        template <int Index, typename... Types>
+        struct TypeIndexer;
+
+        template <int Index, typename T>
+        struct TypeIndexer<Index, T> {
+            using Type = T;
+        };
+
+        template <int Index, typename First, typename... Rest>
+        struct TypeIndexer<Index, First, Rest...> {
+            using Type = typename TypeIndexer<Index - 1, Rest...>::Type;
+        };
+
+    } // namespace detail
+
+    template <int Index, typename... In>
+    using tIndex = detail::TypeIndexer<Index, In...>::Type;
+
+    template <typename Of, typename... In>
+    constexpr static int ctIndexOf = TypeTraits<In...>::template findIndex<Of>();
+
+    template <typename T, typename... In>
+    constexpr static bool ctContains = TypeTraits<In...>::template contains<T>();
+
+    template <typename... Types>
+    constexpr static int ctAmount = TypeTraits<Types...>::amount();
+
+    template <template <typename> typename Trait, typename... Types>
+    constexpr static bool ctAllSatisfied = TypeTraits<Types...>::template all<Trait>();
+
+    namespace detail {
+
+        template <typename T>
+        struct Reference {
+            constexpr static bool cValue = false;
+        };
+
+        template <typename T>
+        struct Reference<T&> {
+            constexpr static bool cValue = true;
+        };
+
+        template <typename T>
+        struct Reference<T&&> {
+            constexpr static bool cValue = true;
+        };
+
+    } // namespace detail
+
+    template <typename T>
+    struct TypeTraits<T>::Reference : detail::Reference<T> { };
+
+    template <typename T>
+    struct TypeTraits<T>::Integral : Bool<ctContains<T,
+                                         bool,
+                                         char,
+                                         signed char,
+                                         unsigned char,
+                                         short,
+                                         unsigned short,
+                                         int,
+                                         unsigned int,
+                                         long,
+                                         unsigned long,
+                                         long long,
+                                         unsigned long long>> { };
+
+    template <typename T>
+    struct TypeTraits<T>::FloatingPoint : Bool<ctContains<T,
+                                              float,
+                                              double,
+                                              long double>> { };
+
+    namespace detail {
+
+        template <typename T>
+        struct RemoveReference {
+            using Type = T;
+        };
+
+        template <typename T>
+        struct RemoveReference<T&> {
+            using Type = T;
+        };
+
+        template <typename T>
+        struct RemoveReference<T&&> {
+            using Type = T;
+        };
+
+    } // namespace detail
+
+    template <typename T>
+    struct TypeTraits<T>::RemoveReference : detail::RemoveReference<T> { };
+
+    namespace detail {
+
+        template <typename T>
+        struct PrintfFormatVerbose;
+
+        template <>
+        struct PrintfFormatVerbose<float> {
+            static constexpr const char cValue[] = "f32: %f";
+        };
+
+        template <>
+        struct PrintfFormatVerbose<double> {
+            static constexpr const char cValue[] = "f64: %lf";
+        };
+
+        template <>
+        struct PrintfFormatVerbose<char*> {
+            static constexpr const char cValue[] = "\"%s\"";
+        };
+        template <>
+        struct PrintfFormatVerbose<const char*> {
+            static constexpr const char cValue[] = "\"%s\"";
+        };
+
+        template <>
+        struct PrintfFormatVerbose<void*> {
+            static constexpr const char cValue[] = "ptr: %p";
+        };
+        template <>
+        struct PrintfFormatVerbose<const void*> {
+            static constexpr const char cValue[] = "ptr: %p";
+        };
+
+    } // namespace detail
+
+    template <typename T>
+    struct TypeTraits<T>::PrintfFormatVerbose : detail::PrintfFormatVerbose<tRemoveConst<T>> { };
+
+} // namespace hk::util
--- hakkun/core/include/hk/types.h
+++ hakkun/core/include/hk/types.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include "hk/prim/traits/Type.h"
 #if !__GNUC__
 #error ""
 #endif
@@ -7,43 +8,15 @@
 #include <cstddef>
 #include <cstdint>
 #include <cstring>
-#include <type_traits>
 #include <utility>
 
 #define INCLUDE_HK_DETAIL_PLATFORM
 #include "hk/detail/platform.h"
 
-#define hk_alwaysinline __attribute__((always_inline))
-#define hk_noinline __attribute__((noinline))
-#define hk_noreturn __attribute__((noreturn))
-#define section(SECTION) __attribute__((section(#SECTION)))
-#define STRINGIFY(X) #X
-#define STR(X) STRINGIFY(X)
-#define CONCAT_IMPL(x, y) x##y
-#define CONCAT(x, y) CONCAT_IMPL(x, y)
+#define INCLUDE_HK_DETAIL_MACROS
+#include "hk/detail/macros.h"
 
-#define NON_COPYABLE(CLASS)                 \
-    constexpr CLASS(const CLASS&) = delete; \
-    constexpr CLASS& operator=(const CLASS&) = delete
-
-#define NON_MOVABLE(CLASS)             \
-    constexpr CLASS(CLASS&&) = delete; \
-    constexpr CLASS& operator=(CLASS&&) = delete
-
-#ifdef HK_RELEASE
-#define ralwaysinline hk_alwaysinline
-#else
-#define ralwaysinline
-#endif
-
-using u8 = uint8_t;
-using u16 = uint16_t;
-using u32 = uint32_t;
-using u64 = uint64_t;
-using s8 = int8_t;
-using s16 = int16_t;
-using s32 = int32_t;
-using s64 = int64_t;
+#include "hk/prim/traits/Integer.h"
 
 using fu8 = uint_fast8_t;
 using fu16 = uint_fast16_t;
@@ -78,15 +51,6 @@ hk_alwaysinline To pun(From val) {
     return toValue;
 }
 
-constexpr size operator""_B(unsigned long long val) { return val; }
-constexpr size operator""_KB(unsigned long long val) { return val * 1024; }
-constexpr size operator""_MB(unsigned long long val) { return val * 1024 * 1024; }
-constexpr size operator""_GB(unsigned long long val) { return val * 1024 * 1024 * 1024; }
-
-constexpr u64 operator""_ns(unsigned long long val) { return val; }
-constexpr u64 operator""_ms(unsigned long long val) { return val * 1e6; }
-constexpr u64 operator""_sec(unsigned long long val) { return val * 1e9; }
-
 namespace hk {
 
     constexpr bool is32Bit() {
@@ -110,31 +74,6 @@ namespace hk {
 
         constexpr operator u32() const { return value; }
     };
-
-    constexpr size cPageSize = 4_KB;
-
-    template <typename T>
-    constexpr T alignUp(T from, size alignment) { return T((ptr(from) + (alignment - 1)) & ~(alignment - 1)); }
-    template <typename T>
-    constexpr T alignDown(T from, size alignment) { return T(ptr(from) & ~(alignment - 1)); }
-
-    template <typename T>
-    constexpr T alignUpPage(T from) { return alignUp(from, cPageSize); }
-    template <typename T>
-    constexpr T alignDownPage(T from) { return alignDown(from, cPageSize); }
-
-    template <typename T>
-    constexpr bool isAligned(T from, size alignment) { return alignDown(from, alignment) == from; }
-    template <typename T>
-    constexpr bool isAlignedPage(T from) { return alignDown(from, cPageSize) == from; }
-
-    constexpr u64 bit(u8 n) { return 1ULL << n; }
-
-    constexpr u64 bits(u8 n) {
-        if (n == 64)
-            return 0xFFFFFFFFFFFFFFFF;
-        return bit(n) - 1;
-    }
 
     template <typename L>
     class ScopeGuard {
@@ -180,13 +119,13 @@ namespace hk {
 #define defer_if(COND) auto CONCAT(CONCAT(scope_exit_guard_, __LINE__), __COUNTER__) = ::hk::ScopeGuardOnExit(COND) + [&]()
 
     template <typename T, typename Base>
-    concept Derived = std::is_base_of_v<Base, T>;
+    concept Derived = util::ctIsBaseOf<Base, T>;
 
     template <typename T>
-    concept ReferenceType = std::is_reference_v<T>;
+    concept ReferenceType = util::ctIsReference<T>;
 
     template <typename T>
-    concept PointerType = std::is_pointer_v<T>;
+    concept PointerType = util::ctIsPointer<T>;
 
 } // namespace hk
 
