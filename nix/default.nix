41fe960a01265d5da17286817759641a1402df24
Aubrey Taylor <aubrey@hall.ly>
2026-05-14 15:08:48 -0600

    update nix outputs

--- nix/default.nix
+++ nix/default.nix
@@ -1,22 +1,58 @@
-pkgs:
-with pkgs; rec {
-  localPackages = {
-    switch-tools = pkgs.callPackage ./switch-tools.nix {};
+pkgs: with pkgs; rec {
+  dependencies =
+    let
+      python3 = pkgs.python3.withPackages (pypkgs: [
+        pypkgs.pyelftools
+        pypkgs.mmh3
+        pypkgs.lz4
+      ]);
+    in
+    [
+      python3
+      llvmPackages.bintools-unwrapped
+      lld
+      curl
+      cmake
+      ninja
+      pkg-config
+      nixfmt
+    ];
+
+  devShell =
+    mkShellNoCC.override
+      {
+        stdenv = pkgsCross.aarch64-embedded.llvmPackages_20.stdenv;
+      }
+      rec {
+        nativeBuildInputs = dependencies ++ [ build-sail ];
+        LD_LIBRARY_PATH = lib.makeLibraryPath (
+          nativeBuildInputs
+          ++ [
+            llvmPackages_20.clang-unwrapped.lib
+          ]
+        );
+
+        shellHook = ''
+          ${build-sail}/bin/build-sail > /dev/null
+        '';
+      };
+
+  build-sail = stdenv.mkDerivation {
+    name = "build-sail";
+    dontUnpack = true;
+    nativeBuildInputs = [ makeWrapper ];
+    installPhase = ''
+      makeWrapper ${writeShellScript "build-sail-inner" ''
+        cmake -S sys/hakkun/sail -B sys/hakkun/sail/build
+        cmake --build sys/hakkun/sail/build --parallel
+      ''} $out/bin/build-sail \
+        --prefix PATH : ${
+          lib.makeBinPath [
+            cmake
+            gnumake
+            llvmPackages_20.clang
+          ]
+        }
+    '';
   };
-  dependencies = let
-    python3 = pkgs.python3.withPackages (pypkgs: [
-      pypkgs.pyelftools
-      pypkgs.mmh3
-      pypkgs.lz4
-    ]);
-  in [
-    localPackages.switch-tools
-    python3
-    llvmPackages.clang-unwrapped
-    llvmPackages.bintools-unwrapped
-    lld
-    curl
-    cmake
-    ninja
-  ];
 }
