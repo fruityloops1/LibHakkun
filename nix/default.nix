d5e9831bdcb8d2cdc6fa484381d964d72e775f16
Aubrey Taylor <aubrey@hall.ly>
2026-05-19 15:51:12 -0600

    more nix fixes

--- nix/default.nix
+++ nix/default.nix
@@ -1,4 +1,9 @@
-pkgs: with pkgs; rec {
+pkgs:
+with pkgs;
+let
+  llvm = llvmPackages_20;
+in
+rec {
   dependencies =
     let
       python3 = pkgs.python3.withPackages (pypkgs: [
@@ -9,7 +14,7 @@ pkgs: with pkgs; rec {
     in
     [
       python3
-      llvmPackages.bintools-unwrapped
+      llvm.bintools-unwrapped
       lld
       curl
       cmake
@@ -19,21 +24,36 @@ pkgs: with pkgs; rec {
     ];
 
   devShell =
+    hakkunLocation:
     mkShellNoCC.override
       {
-        stdenv = pkgsCross.aarch64-embedded.llvmPackages_20.stdenv;
+        stdenv = pkgs.stdenvNoCC.override {
+          # no more <arm_neon.h> include errors
+          hasCC = true;
+          cc = wrapCCWith {
+            cc = llvm.clang-unwrapped;
+            gccForLibs = null;
+            extraPackages = [ llvm.compiler-rt ];
+            extraBuildCommands = ''
+              rsrc="$out/resource-root"
+              mkdir "$rsrc"
+              echo "-resource-dir=$rsrc" >> $out/nix-support/cc-cflags
+              echo "export NIX_CC_WRAPPER_SUPPRESS_TARGET_WARNING=1" >> $out/nix-support/setup-hook
+              ln -s "${llvm.clang-unwrapped.lib}/lib/clang/20/include" "$rsrc"
+              ln -s "${llvm.compiler-rt.out}/lib" "$rsrc/lib"
+              ln -s "${llvm.compiler-rt.out}/share" "$rsrc/share"
+            '';
+          };
+          allowedRequisites = null;
+        };
       }
       rec {
         nativeBuildInputs = dependencies ++ [ build-sail ];
-        LD_LIBRARY_PATH = lib.makeLibraryPath (
-          nativeBuildInputs
-          ++ [
-            llvmPackages_20.clang-unwrapped.lib
-          ]
-        );
+        LD_LIBRARY_PATH = lib.makeLibraryPath nativeBuildInputs;
+        hardeningDisable = [ "all" ];
 
         shellHook = ''
-          ${build-sail}/bin/build-sail > /dev/null
+          ${build-sail}/bin/build-sail ${hakkunLocation} > /dev/null
         '';
       };
 
@@ -43,8 +63,8 @@ pkgs: with pkgs; rec {
     nativeBuildInputs = [ makeWrapper ];
     installPhase = ''
       makeWrapper ${writeShellScript "build-sail-inner" ''
-        cmake -S sys/hakkun/sail -B sys/hakkun/sail/build
-        cmake --build sys/hakkun/sail/build --parallel
+        cmake -S $1/sail -B $1/sail/build
+        cmake --build $1/sail/build --parallel 
       ''} $out/bin/build-sail \
         --prefix PATH : ${
           lib.makeBinPath [
